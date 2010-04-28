e444f237b9a97e058b0c6d183ae18f37d510e6af
Josh Partlow <jpartlow@opensourcery.com>
2010-04-27 17:35:42 -0700

    added expected/provided_section fields to content_errors and marked them as serialized in model

--- app/models/content_error.rb
+++ app/models/content_error.rb
@@ -1,7 +1,11 @@
+# Keeps state for any error produced by Validators when validating a particular test plan.
 class ContentError < ActiveRecord::Base
   belongs_to :test_plan
 
   acts_as_tree 
+
+  serialize :expected_section, Hash
+  serialize :provided_sections, Array
 
   state_machine :initial => :failed do
     event :pass do
