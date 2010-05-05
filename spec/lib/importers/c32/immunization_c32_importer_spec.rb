932dcb241d6198dd0eff6bfc59ba7fe5a74be2c8
Josh Partlow <jpartlow@opensourcery.com>
2010-05-05 16:52:34 -0700

    fixtures needed for immunixation importer test

--- spec/lib/importers/c32/immunization_c32_importer_spec.rb
+++ spec/lib/importers/c32/immunization_c32_importer_spec.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 
 describe ImmunizationC32Importer do
+  fixtures :all
+
   it "should import an immunization entry in a C32 into an ActiveRecord object" do
     document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/immunizations/lacey_immunizations.xml'))
 
