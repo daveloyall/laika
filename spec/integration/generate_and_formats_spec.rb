bb8eb8d48bfec8fc9f9ec7257d977cc6b56d008a
Josh Partlow <jpartlow@opensourcery.com>
2010-05-10 13:27:50 -0700

    beginnings of a failing integration test for generate and format using webrat and some manual http submission to get around some javascript and ajax.

---
+++ spec/integration/generate_and_formats_spec.rb
@@ -0,0 +1,20 @@
+require 'integration_spec_helper'
+
+describe "GenerateAndFormats" do
+
+  before do
+    @user = login
+    @test_plan = create_test_plan(:type => "GenerateAndFormatPlan")
+    @c32_file = File.new(File.join(RAILS_ROOT, 'spec', 'test_data', 'joe_c32.xml'))
+  end
+
+  it "should be able to create a generate and format test" do
+    visit vendor_test_plans_path(:vendor_id => @test_plan.vendor)
+    xhr :get, test_action_path(:id => @test_plan.id, :action => :doc_upload)
+    select 'C32 v2.5'
+    attach_file "upload_#{@test_plan.id}", @c32_file.path
+    click_button 'Attach'
+    body.should contain('foo')
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,9 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
 require 'modelfactory'
 require File.expand_path(File.dirname(__FILE__) + '/laika_spec_helper')
-
-# Uncomment the next line to use webrat's matchers
-#require 'webrat/integrations/rspec-rails'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -45,7 +40,7 @@ ModelFactory.configure do
     email { |i| "factoryuser#{i}@example.com" }
     first_name { "Harry" }
     last_name { "Manchester" }
-    password { "secret " }
+    password { "password" }
     password_confirmation { password }
   end
 
