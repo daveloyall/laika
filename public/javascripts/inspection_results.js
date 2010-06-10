bb04eb87b33d27787dea89227708915d105e9f5c
Josh Partlow <jpartlow@opensourcery.com>
2010-06-09 21:42:07 -0700

    G/F ui updates * overall content inspection state is shown in content inspection tab, and updates as error states are changed * fixed the filters * humanized field names

--- public/javascripts/inspection_results.js
+++ public/javascripts/inspection_results.js
@@ -1,72 +1,49 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function(){
-	
-	// Hiding all force pass/fail controls
-	$j(".error_review").hide();
+  
+  // Hiding all force pass/fail controls
+  $j(".error_review").hide();
 
-	// Toggle review controls
-	$j(".manual-review").click(function(event) {
-			$j(this).text($j(this).text() == 'DETAILS' ? 'CLOSE' : 'DETAILS');
-			$j(this).next($j(".error_review")).slideToggle("slow");
-			return false;
-	})
+  // Toggle review controls
+  $j(".manual-review").click(function(event) {
+      $j(this).text($j(this).text() == 'DETAILS' ? 'CLOSE' : 'DETAILS');
+      $j(this).next($j(".error_review")).slideToggle("slow");
+      return false;
+  })
 
-	// FILTERING
+  // FILTERING
   $j("#filter input:checkbox").click(function(e) {
-      var pass = $j("tr, td").filter('.pass');
-			var fail = $j("tr, td").filter('.failed');
-			var review = $j("tr, td").filter('.review');
-			var nottested = $j("tr, td").filter('.not_tested');
-      
-			// If passed checkbox is checked
-      if ($j("#fieldPass").is(":checked")) {
-			//show passed fields
-				pass.show();
-			} else {
-				pass.hide();
-			}
-			
-			// If review checkbox is checked
-      if ($j("#fieldReview").is(":checked")) {
-			//show passed fields
-				review.show();
-			} else {
-				review.hide();
-			}
-			
-			// If fail checkbox is checked
-      if ($j("#fieldFailed").is(":checked")) {
-			//show passed fields
-				fail.show();
-			} else {
-				fail.hide();
-			}
-			
-			// If not tested checkbox is checked
-      if ($j("#fieldNotTested").is(":checked")) {
-			//show passed fields
-				nottested.show();
-			} else {
-				nottested.hide();
-			}
+    var input = e.target;
+    var type = input.name;
+    var rows = $j("tr." + type + " td");
+    // If passed checkbox is checked
+    if (input.checked) {
+      //show passed fields
+      rows.show();
+    } else {
+      rows.hide();
+    }
   });
 
-	// SCROLLING	
-	//$j('a.error_link').click(function() {
-	//		var $errorid = (this).attr("id");
-	//		var $target = $j('xml_frame').find('div:attr(id, $errorid)');
-	//    $j('#xml_frame').scrollTo( $target, {axis:'x'});
-	//});
+  set_overall_content_inspection_status();
+
+  // SCROLLING  
+  //$j('a.error_link').click(function() {
+  //    var $errorid = (this).attr("id");
+  //    var $target = $j('xml_frame').find('div:attr(id, $errorid)');
+  //    $j('#xml_frame').scrollTo( $target, {axis:'x'});
+  //});
   
 });
 
 function update_error_class(content_error_id) {
 //  console.log('update');
 //  console.log(content_error_id);
-  var selector = '#' + content_error_id + ' .result';
+  var selector = '#' + content_error_id;
   var select_control = $j(selector + ' select');
   set_error_class($j(selector), select_control.val());
+  set_overall_content_inspection_status();
 }
 
 function set_error_class(element, value) {
@@ -83,4 +60,23 @@ function reset_error(content_error_id) {
 //  console.log('reset');
 //  console.log(content_error_id);
   alert('failed to set state for ' + content_error_id)
+}
+
+function set_overall_content_inspection_status() {
+  var error_rows = $j('.scrollContent tr');
+  var inspection_status = $j('#content_inspection_status');
+  var status_code = $j('#content_inspection_status .status_code');
+  console.log(error_rows)
+  console.log(inspection_status)
+  console.log(status_code)
+  if (error_rows.hasClass('review')) {
+    status_code.text('Pending');
+    set_error_class(inspection_status, 'review');
+  } else if (error_rows.hasClass('failed')) {
+    status_code.text('Failed');
+    set_error_class(inspection_status, 'failed');
+  } else {
+    status_code.text('Passed');
+    set_error_class(inspection_status, 'passed');
+  } 
 }
