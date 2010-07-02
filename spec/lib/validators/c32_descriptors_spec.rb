42d0a18728f24f6a945c1af0b3254303893bc4e6
Josh Partlow <jpartlow@opensourcery.com>
2010-07-02 11:48:32 -0700

    * components() creates a ComponentDefinition which stores the definition as a Proc so that we can instantiate completely fresh copies of a Component whenever needed for validation or other purposes. * attribute() shortcut for a field whose locator is its key as an xpath attribute * attach() associates an xml node with a descriptor, extracts the associated value via the locator and descends and attaches children so that the entire descriptor tree has associated values * NodeManipulation and Traversal methods * Logging methods * template_id/locator generation * matches_by, matches_key generation for repeating sections * begun to alter c32_validator to use descriptors

--- spec/lib/validators/c32_descriptors_spec.rb
+++ spec/lib/validators/c32_descriptors_spec.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe Validators::C32Descriptors do
 
   it "should produce a descriptor hash" do
-    descriptors = Validators::C32Descriptors.descriptors
-    pp descriptors
-
-    pp "\n\n\nneed to figure out SectionArray is both hash and array...\n\n\n"
+    languages = Validators::C32Descriptors.get_component(:languages)
+    languages.should be_kind_of ComponentDescriptors::Component
+    repeating_section_template = languages.values.first
+    repeating_section_template.values.first.size.should == 3
   end
 
 end
